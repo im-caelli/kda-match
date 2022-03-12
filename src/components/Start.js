f0b8777646f4e18e8e9330420c4c0eebd5aa4566
im-caelli <im.caelli@gmail.com>
2022-03-12 12:04:12 -0500

    Tweak start modal styles

--- src/components/Start.js
+++ src/components/Start.js
@@ -17,7 +17,7 @@ export default function Start(props) {
         <div className='modal-start-content-wrapper'>
           <div className='modal-start-content'>
             <h2>K/DA Matching Game</h2>
-            <p>Click a card to flip it. Try to find a matching pair. How many turns would it take you to match them all? 🤔 </p>
+            <p>Click a card to flip it and try to find a matching pair. How many turns would it take you to match them all? 🤔 </p>
             <Button className='modal-start-btn' onClick={props.onHide}>START!</Button>
           </div>
         </div>
