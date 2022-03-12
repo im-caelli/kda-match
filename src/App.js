4f69db7360981ef78ed45f1c7af9ec8817d2e704
im-caelli <im.caelli@gmail.com>
2022-03-11 20:38:25 -0500

    trouble shoot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
+const cards = [
+  { 'type': 'ahri', 'src': 'img/ahri.png', matched:false },
+  { 'type': 'akali', 'src': 'img/akali.png', matched:false },
+  { 'type': 'evelynn', 'src': 'img/evelynn.png', matched:false },
+  { 'type': 'kaisa', 'src': 'img/kaisa.png', matched:false },
+  { 'type': 'seraphine', 'src': 'img/seraphine.png', matched:false },
+  { 'type': 'group', 'src': 'img/group.png', matched:false }
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+
+  const shuffleCards = () => {
+
+    const shuffledCards = [...cards, ...cards]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+  }
+
+  console.log(cards)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Hello!</p>
+     <p><button onClick={shuffleCards}>New Game</button></p>
     </div>
   );
 }
